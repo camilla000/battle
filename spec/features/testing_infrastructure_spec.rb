3fb54268e5439abce68ed65f5469f64ab5d3da50
Camilla <camilla.lima000@gmail.com>
2021-06-10 16:47:31 +0100

    Add entering players

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,12 @@
-feature "Testing infrastructure" do
-  scenario "Can run app and check page content" do
-		visit ('/')
-		expect(page).to have_content "Testing infrastructure working!"  
-	end
+feature 'describe battle' do
+  scenario 'can show player names on screen' do
+    visit('/')
+    fill_in('player_1_name', with: 'Camilla')
+    fill_in('player_2_name', with: 'Mary')
+    click_button('Submit')
+
+    save_and_open_page
+
+    expect(page).to have_content('Camilla vs. Mary')
+  end
 end
